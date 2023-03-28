a8bb31823dbac12b3b361ef8b2820248ad074bb5
EswarAdithyaB <2100039088cse@gmail.com>
2023-03-28 19:54:53 +0530

    Eswar Test1

--- src/styles/GlobalComponents/Button.js
+++ src/styles/GlobalComponents/Button.js
@@ -6,6 +6,7 @@ const Button = (props) => (
   <ButtonBack alt={props.alt} form={props.form} disabled={props.disabled}>{props.children}
     <ButtonFront alt={props.alt} onClick={props.onClick} disabled={props.disabled}>{props.children}</ButtonFront>
   </ButtonBack>
+  <h1>Hi</h1>
 );
 
 export default Button
