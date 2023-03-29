5f95fdf7b695a762b56f015850932834a8975699
EswarAdithyaB <2100039088cse@gmail.com>
2023-03-29 12:46:26 +0530

    Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,23 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Hello Techies! <br />
+          I am Bandi Eswar Adithya
+        </SectionTitle>
+        <SectionText>
+        Welcome to my portfolio! As a Computer Science engineering<br/>
+        student with a passion for software engineering, I'm excited<br/> 
+        to share with you some of the projects I have worked on and<br/>
+        the skills I have developed. 
+        </SectionText>
+        <Button onClick={props.handleClick}>Learn More</Button>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
